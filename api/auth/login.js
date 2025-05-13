06e7657e2dd23b594001af7a96f343ab3a2c71df
Yash Kumar <144141617+Yashkumar23092003@users.noreply.github.com>
2025-05-13 22:14:31 +0530

    Update login.js

--- api/auth/login.js
+++ api/auth/login.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ success: false, message: 'Method not allowed' });
   }
-
+//
   try {
     const { email, password } = req.body;
 
